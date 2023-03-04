551e193355297abd2552157cf9357dfbff05d9f8
Vishnu Bharathi <vishnubharathi04@gmail.com>
2023-03-04 21:28:28 +0530

    feat(scaler.docker): add scaler up logic

--- go.mod
+++ go.mod
@@ -8,13 +8,25 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v23.0.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/knadh/koanf/parsers/toml v0.1.0 // indirect
 	github.com/knadh/koanf/providers/file v0.1.0 // indirect
 	github.com/knadh/koanf/v2 v2.0.0 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 )
--- internal/scaler/docker/docker.go
+++ internal/scaler/docker/docker.go
@@ -5,6 +5,10 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/knadh/koanf/v2"
 	"github.com/scriptnull/waymond/internal/event"
 	"github.com/scriptnull/waymond/internal/scaler"
@@ -24,9 +28,58 @@ func (s *Scaler) Type() scaler.Type {
 }
 
 func (s *Scaler) Register(ctx context.Context) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+	cli.NegotiateAPIVersion(ctx)
+
 	eventBus := ctx.Value("eventBus").(event.Bus)
 	eventBus.Subscribe(fmt.Sprintf("scaler.%s", s.id), func() {
-		fmt.Println("event received inside docker scaler. this will perform an autoscale")
+		fmt.Printf("scaler.%s.start \n", s.id)
+
+		imageFullName := fmt.Sprintf("%s:%s", s.imageName, s.imageTag)
+
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+			Filters: filters.NewArgs(
+				filters.KeyValuePair{
+					Key:   "status",
+					Value: "running",
+				},
+				filters.KeyValuePair{
+					Key:   "ancestor",
+					Value: imageFullName,
+				},
+			),
+		})
+		if err != nil {
+			fmt.Println("error listing containers", err)
+			return
+		}
+
+		currentCount := len(containers)
+		if currentCount < s.count {
+			fmt.Printf("scaler.%s current count: %d, desired count: %d \n", s.id, currentCount, s.count)
+			remainingCount := s.count - currentCount
+
+			for i := 0; i < remainingCount; i++ {
+				c, err := cli.ContainerCreate(ctx, &container.Config{Image: imageFullName}, nil, nil, nil, "")
+				if err != nil {
+					fmt.Printf("scaler.%s.error: %s \n", s.id, err)
+					return
+				}
+				fmt.Printf("scaler.%s.container.created: %s \n", s.id, c.ID)
+
+				err = cli.ContainerStart(ctx, c.ID, types.ContainerStartOptions{})
+				if err != nil {
+					fmt.Printf("scaler.%s.error: %s \n", s.id, err)
+					return
+				}
+				fmt.Printf("scaler.%s.container.started: %s \n", s.id, c.ID)
+			}
+		}
+
+		fmt.Printf("scaler.%s.done \n", s.id)
 	})
 	return nil
 }
