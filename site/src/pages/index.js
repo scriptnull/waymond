80f6ee27ebf5b82c144a3dd705b0e1df01adc8d3
Vishnu Bharathi P <vishnubharathi04@gmail.com>
2023-04-14 03:04:53 +0530

    remove unwanted content from home page

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* TODO */}
       </main>
     </Layout>
   );
