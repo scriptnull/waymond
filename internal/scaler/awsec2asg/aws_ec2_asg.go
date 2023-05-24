1d9213f18252a14eac92164f7667ca48d0e237a7
Vishnu Bharathi <vishnubharathi04@gmail.com>
2023-05-25 05:04:08 +0530

    success

--- internal/scaler/awsec2asg/aws_ec2_asg.go
+++ internal/scaler/awsec2asg/aws_ec2_asg.go
@@ -60,17 +60,15 @@ type LaunchTemplate struct {
 }
 
 type LaunchTemplateSpecification struct {
-	LaunchTemplateId   *string                        `koanf:"launch_template_id" json:"launch_template_id"`
-	LaunchTemplateName *string                        `koanf:"launch_template_name" json:"launch_template_name"`
-	Version            *string                        `koanf:"version" json:"version"`
-	CreateIfNotExists  *bool                          `koanf:"create_if_not_exists" json:"create_if_not_exists"`
-	Spec               *ec2.CreateLaunchTemplateInput `koanf:"spec" json:"spec"`
+	LaunchTemplateId   *string `koanf:"launch_template_id" json:"launch_template_id"`
+	LaunchTemplateName *string `koanf:"launch_template_name" json:"launch_template_name"`
+	Version            *string `koanf:"version" json:"version"`
 }
 
 type LaunchTemplateOverrides struct {
 	// TODO: too much of struct nesting in InstanceRequirements
 	// so add it here when we actually need it.
-	// InstanceRequirements        *InstanceRequirements        `koanf:"instance_requirements"`
+	// InstanceRequirements *InstanceRequirements `koanf:"instance_requirements"`
 
 	InstanceType                *string                      `koanf:"instance_type" json:"instance_type"`
 	LaunchTemplateSpecification *LaunchTemplateSpecification `koanf:"launch_template_specification" json:"launch_template_specification"`
@@ -96,17 +94,66 @@ func (s *Scaler) Register(ctx context.Context) error {
 		s.log.Debugf("data: %+v\n", string(data))
 
 		var inputData struct {
-			ASGName        string                       `json:"asg_name"`
-			DesiredCount   int64                        `json:"desired_count"`
-			Tags           []ASGTag                     `json:"tags"`
-			LaunchTemplate *LaunchTemplateSpecification `json:"launch_template"`
-			Overrides      []LaunchTemplateOverrides    `json:"launch_template_overrides"`
+			ASGName                   string                       `json:"asg_name"`
+			DesiredCount              int64                        `json:"desired_count"`
+			Tags                      []ASGTag                     `json:"tags"`
+			BaseLaunchTemplate        *LaunchTemplateSpecification `json:"base_launch_template"`
+			LaunchTemplateVersionOpts struct {
+				AmiID *string `json:"ami_id"`
+			} `json:"launch_template_version_options"`
+			Overrides []LaunchTemplateOverrides `json:"launch_template_overrides"`
 		}
 
 		err := json.Unmarshal(data, &inputData)
 		if err != nil {
 			s.log.Errorf("error: %s \n", err)
 			return
+		}
+
+		// ensure launch template and its versions are available for the autoscaling group to make use of
+		var preferredLaunchTemplateVersion *ec2.LaunchTemplateVersion
+		if inputData.LaunchTemplateVersionOpts.AmiID != nil {
+			if inputData.BaseLaunchTemplate == nil {
+				s.log.Error("'base_launch_template' is required input for the aws_ec2_asg scaler if 'launch_template_version_options' is mentioned \n")
+				return
+			}
+			s.log.Verbosef("querying for a launch template version with AMI ID: %s \n", *inputData.LaunchTemplateVersionOpts.AmiID)
+			imageId := "image-id"
+			ltvs, err := ec2svc.DescribeLaunchTemplateVersions(&ec2.DescribeLaunchTemplateVersionsInput{
+				LaunchTemplateId:   inputData.BaseLaunchTemplate.LaunchTemplateId,
+				LaunchTemplateName: inputData.BaseLaunchTemplate.LaunchTemplateName,
+				Filters: []*ec2.Filter{
+					{
+						Name:   &imageId,
+						Values: []*string{inputData.LaunchTemplateVersionOpts.AmiID},
+					},
+				},
+			})
+			if err != nil {
+				s.log.Errorf("error querying for a launch template version containing AMI ID (%s): %s \n", *inputData.LaunchTemplateVersionOpts.AmiID, err)
+				return
+			}
+			if ltvs != nil && len(ltvs.LaunchTemplateVersions) > 0 {
+				ltv := ltvs.LaunchTemplateVersions[0]
+				s.log.Verbosef("found a launch template version containing the given AMI ID: %s, launch template version: %d \n", *inputData.LaunchTemplateVersionOpts.AmiID, *ltv.VersionNumber)
+				preferredLaunchTemplateVersion = ltv
+			} else {
+				s.log.Verbosef("unable to find a launch template version containing the given AMI ID: %s, so creating one. \n", *inputData.LaunchTemplateVersionOpts.AmiID)
+				ltv, err := ec2svc.CreateLaunchTemplateVersion(&ec2.CreateLaunchTemplateVersionInput{
+					LaunchTemplateName: inputData.BaseLaunchTemplate.LaunchTemplateName,
+					LaunchTemplateId:   inputData.BaseLaunchTemplate.LaunchTemplateId,
+					SourceVersion:      inputData.BaseLaunchTemplate.Version,
+					LaunchTemplateData: &ec2.RequestLaunchTemplateData{
+						ImageId: inputData.LaunchTemplateVersionOpts.AmiID,
+					},
+				})
+				if err != nil {
+					s.log.Error("error while creating a launch template version: %s", err)
+					return
+				}
+				s.log.Verbosef("created a launch template version containing the given AMI ID: %s, launch template version: %d \n", *inputData.LaunchTemplateVersionOpts.AmiID, ltv.LaunchTemplateVersion.VersionNumber)
+				preferredLaunchTemplateVersion = ltv.LaunchTemplateVersion
+			}
 		}
 
 		var maxRecords int64 = 1
@@ -142,10 +189,12 @@ func (s *Scaler) Register(ctx context.Context) error {
 				DefaultCooldown:   s.DefaultCooldown,
 				PlacementGroup:    s.PlacementGroup,
 			}
+
 			if len(s.VpcZoneIdentifier) > 0 {
 				vpcZoneIdentifiers := strings.Join(s.VpcZoneIdentifier, ",")
 				createAsgInput.VPCZoneIdentifier = &vpcZoneIdentifiers
 			}
+
 			asgTags := append(s.Tags, inputData.Tags...)
 			if len(asgTags) > 0 {
 				for _, asgTag := range asgTags {
@@ -157,74 +206,82 @@ func (s *Scaler) Register(ctx context.Context) error {
 				}
 			}
 
+			if preferredLaunchTemplateVersion != nil && s.MixedInstancesPolicy == nil {
+				// `LaunchTemplate` could be set only when `MixedInstancesPolicy` is not specified.
+				stringVersion := fmt.Sprintf("%d", *preferredLaunchTemplateVersion.VersionNumber)
+				createAsgInput.LaunchTemplate = &autoscaling.LaunchTemplateSpecification{
+					LaunchTemplateId: preferredLaunchTemplateVersion.LaunchTemplateId,
+					Version:          &stringVersion,
+				}
+			}
+
 			if s.MixedInstancesPolicy != nil {
-				createAsgInput.MixedInstancesPolicy = &autoscaling.MixedInstancesPolicy{
-					InstancesDistribution: &autoscaling.InstancesDistribution{
+				createAsgInput.MixedInstancesPolicy = &autoscaling.MixedInstancesPolicy{}
+
+				if s.MixedInstancesPolicy.InstanceDistribution != nil {
+					createAsgInput.MixedInstancesPolicy.InstancesDistribution = &autoscaling.InstancesDistribution{
 						OnDemandAllocationStrategy:          s.MixedInstancesPolicy.InstanceDistribution.OnDemandAllocationStrategy,
 						OnDemandBaseCapacity:                s.MixedInstancesPolicy.InstanceDistribution.OnDemandBaseCapacity,
 						OnDemandPercentageAboveBaseCapacity: s.MixedInstancesPolicy.InstanceDistribution.OnDemandPercentAboveBaseCapacity,
 						SpotAllocationStrategy:              s.MixedInstancesPolicy.InstanceDistribution.SpotAllocationStrategy,
 						SpotInstancePools:                   s.MixedInstancesPolicy.InstanceDistribution.SpotInstancePools,
 						SpotMaxPrice:                        s.MixedInstancesPolicy.InstanceDistribution.SpotMaxPrice,
-					},
-					LaunchTemplate: &autoscaling.LaunchTemplate{
+					}
+				}
+
+				if s.MixedInstancesPolicy.LaunchTemplate != nil {
+					createAsgInput.MixedInstancesPolicy.LaunchTemplate = &autoscaling.LaunchTemplate{
 						LaunchTemplateSpecification: &autoscaling.LaunchTemplateSpecification{
 							LaunchTemplateId:   s.MixedInstancesPolicy.LaunchTemplate.LaunchTemplateSpecification.LaunchTemplateId,
 							LaunchTemplateName: s.MixedInstancesPolicy.LaunchTemplate.LaunchTemplateSpecification.LaunchTemplateName,
 							Version:            s.MixedInstancesPolicy.LaunchTemplate.LaunchTemplateSpecification.Version,
 						},
-					},
+					}
+				}
+
+				if preferredLaunchTemplateVersion != nil {
+					// `LaunchTemplate` could be set only when `MixedInstancesPolicy` is not specified.
+					stringVersion := fmt.Sprintf("%d", *preferredLaunchTemplateVersion.VersionNumber)
+					createAsgInput.MixedInstancesPolicy.LaunchTemplate = &autoscaling.LaunchTemplate{
+						LaunchTemplateSpecification: &autoscaling.LaunchTemplateSpecification{
+							LaunchTemplateId: preferredLaunchTemplateVersion.LaunchTemplateId,
+							Version:          &stringVersion,
+						},
+					}
 				}
 			}
 
 			for _, o := range inputData.Overrides {
-				s.log.Debugf("o = %+v \n", o)
-
-				if o.LaunchTemplateSpecification.CreateIfNotExists != nil && *o.LaunchTemplateSpecification.CreateIfNotExists {
-					if o.LaunchTemplateSpecification.Spec == nil {
-						s.log.Error("create_if_not_exists option for launch template expects spec object to be present")
-						return
-					}
-
-					// check if the launch template exists
-					lts, err := ec2svc.DescribeLaunchTemplates(&ec2.DescribeLaunchTemplatesInput{
-						LaunchTemplateNames: []*string{o.LaunchTemplateSpecification.LaunchTemplateName},
-					})
-					if err != nil {
-						if !strings.Contains(err.Error(), "InvalidLaunchTemplateName.NotFoundException") {
-							s.log.Error("error while checking for launch template", err)
-							continue
-						}
-					}
-
-					if lts != nil && len(lts.LaunchTemplates) > 0 {
-						s.log.Debugf("launch template named %s exists, so avoiding the creation of it.\n", *o.LaunchTemplateSpecification.LaunchTemplateName)
-						continue
-					}
-
-					// create a launch template
-					s.log.Verbosef("launch template named %s does not exist, so creating it.\n", *o.LaunchTemplateSpecification.LaunchTemplateName)
-					o.LaunchTemplateSpecification.Spec.LaunchTemplateName = o.LaunchTemplateSpecification.LaunchTemplateName
-					s.log.Debug("launch template spec", o.LaunchTemplateSpecification.Spec)
-					ltOut, err := ec2svc.CreateLaunchTemplate(o.LaunchTemplateSpecification.Spec)
-					if err != nil {
-						s.log.Error("error while creating launch template", err)
-						continue
-					}
-
-					s.log.Verbosef("created a new launch template: %s\n", ltOut)
+				if createAsgInput.MixedInstancesPolicy == nil {
+					createAsgInput.MixedInstancesPolicy = &autoscaling.MixedInstancesPolicy{}
 				}
-			}
 
-			for _, o := range inputData.Overrides {
-				createAsgInput.MixedInstancesPolicy.LaunchTemplate.Overrides = append(createAsgInput.MixedInstancesPolicy.LaunchTemplate.Overrides, &autoscaling.LaunchTemplateOverrides{
-					InstanceType: o.InstanceType,
-					LaunchTemplateSpecification: &autoscaling.LaunchTemplateSpecification{
+				if createAsgInput.MixedInstancesPolicy.LaunchTemplate == nil {
+					createAsgInput.MixedInstancesPolicy.LaunchTemplate = &autoscaling.LaunchTemplate{}
+				}
+
+				override := &autoscaling.LaunchTemplateOverrides{}
+
+				if o.InstanceType != nil {
+					override.InstanceType = o.InstanceType
+				}
+
+				// if preferredLaunchTemplateVersion != nil {
+				// 	stringVersion := fmt.Sprintf("%d", *preferredLaunchTemplateVersion.VersionNumber)
+				// 	override.LaunchTemplateSpecification = &autoscaling.LaunchTemplateSpecification{
+				// 		LaunchTemplateId: preferredLaunchTemplateVersion.LaunchTemplateId,
+				// 		Version:          &stringVersion,
+				// 	}
+				// }
+
+				if o.LaunchTemplateSpecification != nil {
+					override.LaunchTemplateSpecification = &autoscaling.LaunchTemplateSpecification{
 						LaunchTemplateId:   o.LaunchTemplateSpecification.LaunchTemplateId,
 						LaunchTemplateName: o.LaunchTemplateSpecification.LaunchTemplateName,
 						Version:            o.LaunchTemplateSpecification.Version,
-					},
-				})
+					}
+				}
+				createAsgInput.MixedInstancesPolicy.LaunchTemplate.Overrides = append(createAsgInput.MixedInstancesPolicy.LaunchTemplate.Overrides, override)
 			}
 
 			s.log.Verbosef("ASG named %s does not exist. So creating it.\n", *createAsgInput.AutoScalingGroupName)
