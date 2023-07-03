48584df961409378c1acb55dcdcc91561faca08a
ananya-2410 <ananya@hasura.io>
2023-07-03 14:59:53 +0530

    add it to queue config

--- internal/trigger/buildkite/buildkite.go
+++ internal/trigger/buildkite/buildkite.go
@@ -83,6 +83,8 @@ func (t *Trigger) Do(_ []byte) error {
 				// example: job B depends on job C and job C is not present in the build.
 				// Hence, it is better to neglect it during auto-scaling.
 				Waiting int
+				Running int
+				Total   int
 			}
 		} `json:"jobs"`
 	}
@@ -110,10 +112,15 @@ func (t *Trigger) Do(_ []byte) error {
 	}
 
 	for qName, q := range queues {
-		t.log.Debugf("qName: %s, waitingSize: %d \n", qName, q.Scheduled)
+		t.log.Debugf(
+			"qName: %s, scheduledSize: %d, waitingSize: %d, runningSize: %d, totalSize: %d \n",
+			qName, q.Scheduled, q.Waiting, q.Running, q.Total)
 		data := outputData{
 			Queue:              qName,
 			ScheduledJobsCount: q.Scheduled,
+			WaitingJobsCount:   q.Waiting,
+			RunningJobsCount:   q.Running,
+			TotalJobsCount:     q.Total,
 		}
 		rawData, err := json.Marshal(data)
 		if err != nil {
